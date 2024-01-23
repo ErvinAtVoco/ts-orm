6539ff827a64a9dddfe0538d499f867406775752
ErvinAtVoco <ervin.tombak@voco.ee>
2024-01-24 01:14:49 +0200

    Added article and comment models and controllers

--- index.ts
+++ index.ts
@@ -2,6 +2,8 @@ import express, { Express, Request, Response } from "express";
 import mongoose from "mongoose";
 
 import stringsController from "./controllers/strings";
+import articleController from "./controllers/articleController";
+import commentController from "./controllers/commentController";
 
 const app: Express = express();
 
@@ -21,6 +23,8 @@ app.get('/', (req: Request, res: Response) => {
 });
 
 app.use('/', stringsController);
+app.use('/', articleController);
+app.use('/', commentController);
 
 app.listen(3000,() => {
   console.log(`[server]: Server is running at http://localhost:3000`);
